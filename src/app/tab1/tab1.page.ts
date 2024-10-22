666f035b9829c9396372eec5c4e2cbaa6c6c9d10
Camila Droguett <c.droguett@duocuc.cl>
2024-10-22 01:39:47 -0300

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,69 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonFooter
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  alertCircleOutline, 
+  chatbubblesOutline, 
+  personOutline, 
+  locationOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent,
+    IonButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonFooter
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    addIcons({ 
+      alertCircleOutline, 
+      chatbubblesOutline, 
+      personOutline, 
+      locationOutline 
+    });
+  }
+
+  onSOSClick() {
+    console.log('SOS Button clicked');
+    // Aquí implementarías la lógica del botón SOS
+  }
+
+  onChatClick() {
+    console.log('Chat clicked');
+    // Implementar navegación al chat
+  }
+
+  onContactClick() {
+    console.log('Contact clicked');
+    // Implementar navegación a contactos
+  }
+
+  onLocationClick() {
+    console.log('Location clicked');
+    // Implementar navegación a ubicación
+  }
 }
